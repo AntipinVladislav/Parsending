77bdb89844d3ca7560d9fd8ef851c0a9365dbc7b
antivlad63 <antivlad63@yandex.ru>
2023-10-25 23:51:45 +0400

    Barebones but healthy

--- Parsec.py
+++ Parsec.py
@@ -4,19 +4,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pickle
 
-edge_driver_path = 'C:\Bydon (piton)\MicrosoftWebDriver.exe'
-service_1 = Service(edge_driver_path)
 
-service_1.start()
 
-driver = webdriver.Edge(edge_driver_path)
+driver = webdriver.Edge()
 
 driver.get('https://yandex.ru/images/search?text=cat')
-Cat = driver.find_element(By.CLASS_NAME, 'Cat')
+#Cat = driver.find_element(By.CLASS_NAME, 'Cat')
 
-with open("saved_image.pickle", "wb") as f:
-    # Dump the image data into the file using pickle
-    pickle.dump(Cat, f)
+
+
+input()
 
 driver.quit()
-
