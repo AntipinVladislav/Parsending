82d6c6d684ca3d089ebc2f4121a03a0fa697eb3d
antivlad63 <antivlad63@yandex.ru>
2023-10-25 23:52:54 +0400

    Barebones Clean

--- Parsec.py
+++ Parsec.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import pickle
+
 
 
 
